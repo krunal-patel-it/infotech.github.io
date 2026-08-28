8d90ef4ccced496d927e85bad3d148a170aa4eb1
krunal-patel-it <krunal.patel@sdjgroup.org>
2026-08-29 01:16:25 +0530

    Update config.js

--- mcq/student/js/config.js
+++ mcq/student/js/config.js
@@ -5,7 +5,7 @@
  */
 const CONFIG = {
   // PASTE the SAME Web App URL as in admin/js/config.js
-  API_URL: 'https://script.google.com/macros/s/AKfycbxrCFyd5fANp9xJVx0rWn9KJ4jFSi1VB06UMSsiTxk4BZw1ZSaNxrdAwrjJihdIvFtJ8w/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycby41YZmH-QaHHXlcj0AZabC99Bulvxg4z57WJTG-KfP39mTjApApuWwdeeyk5IIDkSk1w/exec',
 
   // Must be true for multi-device sync
   USE_GOOGLE_SHEETS: true,
